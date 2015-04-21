67162e8274306f3f5c44449bb9f0b49da32111c5
Razvan Ilin <razvanilin@uk.ibm.com>
2015-04-21 11:07:31 +0100

    Fixed the bower dependencies

--- client/test/karma.conf.js
+++ client/test/karma.conf.js
@@ -27,6 +27,8 @@ module.exports = function(config) {
       'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/lodash/lodash.js',
+      'bower_components/restangular/dist/restangular.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
