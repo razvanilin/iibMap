b57f095bab6c34692bb69aae0b56b297446c3557
Razvan Ilin <razvanilin@uk.ibm.com>
2015-04-21 16:22:20 +0100

    The broker/getJson route gets the layout of the brokers and the right naming on all the nodes

--- server/controllers/BrokerController.js
+++ server/controllers/BrokerController.js
@@ -11,87 +11,98 @@ module.exports = function(app, route) {
     var Broker = restful.model(
         'broker',
         app.models.broker
-    ).methods(['get', 'put', 'post', 'delete']
-    ).before('get', function(req, res, next) {
+    ).methods(['get', 'put', 'post', 'delete']).before('get', function(req, res, next) {
         // workaround for when ther user requests all the documents
         // should work without, but it doesn't - TODO: find out why
         if (req.params.id.length < 1)
             Broker.find().exec(function(err, users) {
                 res.send(users);
             });
-        else 
+        else
             next();
-    }
-    ).route('/getJson', ['get'], iibAPI);
+    });
 
     Broker.register(app, route);
 
-    app.get('/broker/getJson', iibAPI);
+    app.get('/broker/getJson', iibLayout);
 
     // get json data from IIB API
-    function iibAPI (req, res, next) {
+    function iibLayout(req, res, next) {
 
-        var path = "/apiv1/executiongroups/?depth=3";
+        // get all the brokers from the database
+        Broker.find().exec(function(err, brokers) {
 
-        var options = {
-            url: "",
-            method: "GET",
-            headers: {
-                "Accept": "application/json"
+            var calls = [];
+            for (var i=0; i<brokers.length; i++) {
+                var brokerHost = "http://"+brokers[i]['host']+":"+brokers[i]['port'];
+                calls.push(brokerHost);
             }
-        }
 
-        var calls = ["http://localhost:4415", "http://localhost:4417"];
-        var index = 1;
 
-        var callBack = function(error, resp, body) {
-            var responseString = JSON.parse(body);
-            
-            var brokerData = {
-                "id": "broker" + Math.floor((Math.random() * 100) + 1),
-                "name": "BrokerPrime" + Math.floor((Math.random() * 100) + 1),
-                "children": []
-            };
+            // path used on the external API
+            var path = "/apiv1/?depth=4";
 
-            for (var j = 0; j < responseString['executionGroup'].length; j++) {
-                //var tempData = [];
-                var tempData = {
-                    'id': 'eg' + Math.floor((Math.random() * 100) + 1),
-                    'name': responseString['executionGroup'][j]['name'] + Math.floor((Math.random() * 100) + 1),
-                    'children': []
-                };
-                //tempData['children'] = [];
-                var chartIndex = 1;
-                for (var i = 0; i < responseString['executionGroup'][j]['messageFlows']['messageFlow'].length; i++) {
-                    var messageFlow = {
-                        "id": "flow" + chartIndex,
-                        "name": responseString['executionGroup'][j]['messageFlows']['messageFlow'][i]['name'] + Math.floor((Math.random() * 100) + 1),
-                        "size": 3000
-                    };
-                    tempData['children'].push(messageFlow);
-                    //console.log(tempData);
-                    chartIndex++;
+            // options used when making the call
+            var options = {
+                url: "",
+                method: "GET",
+                headers: {
+                    "Accept": "application/json"
                 }
-                brokerData['children'].push(tempData);
             }
-            //console.log(index);
-            chartData['children'].push(brokerData);
 
-            if (index == (calls.length)) {
-                res.send(chartData);
-                index = 1;
-                chartData = {
-                    "name": "IIB",
+            //var calls = ["http://localhost:4415", "http://localhost:4417"];
+            var index = 1;
+
+            var callBack = function(error, resp, body) {
+                var responseString = JSON.parse(body);
+
+                var brokerData = {
+                    "id": "broker",
+                    "type": responseString['type'],
+                    "name": responseString['name'],
                     "children": []
                 };
-            }
-            index++;
-        }
 
-        for (var i = 0; i < calls.length; i++) {
-            options['url'] = calls[i] + path;
-            request(options, callBack);
-        }
+                // Go through all the execution groups 
+                for (var j = 0; j < responseString['executionGroups']['executionGroup'].length; j++) {
+                    var tempData = {
+                        'id': 'eg',
+                        'type': responseString['executionGroups']['type'],
+                        'name': responseString['executionGroups']['executionGroup'][j]['name'],
+                        'children': []
+                    };
+                    var chartIndex = 1;
+                    for (var i = 0; i < responseString['executionGroups']['executionGroup'][j]['messageFlows']['messageFlow'].length; i++) {
+                        var messageFlow = {
+                            "id": "flow" + chartIndex,
+                            "type": responseString['executionGroups']['executionGroup'][j]['messageFlows']['type'],
+                            "name": responseString['executionGroups']['executionGroup'][j]['messageFlows']['messageFlow'][i]['name'],
+                            "size": Math.floor((Math.random() * 5000) + 1000)
+                        };
+                        tempData['children'].push(messageFlow);
+                        chartIndex++;
+                    }
+                    brokerData['children'].push(tempData);
+                }
+                chartData['children'].push(brokerData);
+
+                if (index == (calls.length)) {
+                    res.send(chartData);
+                    index = 1;
+                    chartData = {
+                        "name": "IIB",
+                        "children": []
+                    };
+                }
+                index++;
+            }
+
+            for (var i = 0; i < calls.length; i++) {
+                options['url'] = calls[i] + path;
+                request(options, callBack);
+            }
+        });
 
     }
 
