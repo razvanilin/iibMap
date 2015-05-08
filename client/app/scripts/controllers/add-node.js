186fa4267e4497a6c2bf5236e4ff5db1c88d236f
Razvan Ilin <razvanilin@uk.ibm.com>
2015-05-08 16:40:14 +0100

    Completed the add-node view and updated the other pieces of code to comply with the new addition

--- client/app/scripts/controllers/add-node.js
+++ client/app/scripts/controllers/add-node.js
@@ -10,4 +10,15 @@
 angular.module('iibHeatMapApp')
   .controller('AddNodeCtrl', function ($scope) {
     $scope.viewAddNode = true;
+    $scope.node = {
+    	host: '',
+    	port: '',
+    	ssl: false,
+    	password: '',
+    	username: ''
+    };
+
+    $scope.addNode = function() {
+    	console.log($scope.node);
+    }
   });
